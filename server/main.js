62c85c19e489d230020bb5d4bba7a3113664adf8
kanadenipun <nipunk@thoughtworks.com>
2017-05-06 10:06:21 +0530

    Nipun/lokesh added endpoint to insert gpslog into mongo

--- server/main.js
+++ server/main.js
@@ -1,5 +1,30 @@
-import { Meteor } from 'meteor/meteor';
+Router.map(function() {
+    this.route('logVehiclePosition', {
+        path: '/api/vehicle/log',
+        where: 'server',
+        action: function() {
+            // GET, POST, PUT, DELETE
+            var requestMethod = 'GET';
+            // Data from a POST request
 
-Meteor.startup(() => {
-  // code to run on server at startup
+            var vehicleID = this.params.query.vehicleID;
+            var latitude = this.params.query.latitude;
+            var longitude = this.params.query.longitude;
+            var speed = this.params.query.speed;
+            var heading = this.params.query.heading;
+            var accuracy = this.params.query.accuracy;
+
+            GPSLog.insert({
+                vehicleID : vehicleID,
+                latitude: latitude,
+                longitude: longitude,
+                speed: speed,
+                heading: heading,
+                accuracy: accuracy
+            });
+
+            this.response.statusCode = 200; 
+            this.response.end( 'success' );
+        }
+    });
 });
