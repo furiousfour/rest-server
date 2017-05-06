76803e6072fba31b626de8457ab38f0c5cd97787
psudha3 <psudha3@gmail.com>
2017-05-06 12:11:52 +0530

    padma/lokesh making calls with hardcoded number

--- server/main.js
+++ server/main.js
@@ -1,3 +1,4 @@
+
 Router.map(function() {
     this.route('logVehiclePosition', {
         path: '/api/vehicle/log',
@@ -26,6 +27,33 @@ Router.map(function() {
             });
 
             this.response.statusCode = 200;
+            this.response.end( 'success' );
+        }
+    });
+
+    this.route('makePhoneCalls', {
+        path: 'api/makeCall',
+        where: 'server',
+        action: function() {
+           // GET, POST, PUT, DELETE
+            var requestMethod = 'GET';
+            // Data from a POST request
+
+            var phoneNumber = this.params.query.phoneNumber; 
+            var account_sid = "AC60d3c4a2882479b57c43890bb41b9300";
+            var auth_token = "d7c478a80271f72f704ad0786f505a0b";
+            var client = Npm.require('twilio')(account_sid, auth_token);
+
+            client.calls.create({
+                to:phoneNumber,
+                from:'+919790244477',
+                url:'http://52.77.53.23:9000/test/test.xml'
+            }, function(err, data) {
+                console.log('This call\'s unique ID is: ' + call.sid);
+                console.log('This call was created at: ' + call.dateCreated);
+            });
+
+            this.response.statusCode = 200; 
             this.response.end( 'success' );
         }
     });
